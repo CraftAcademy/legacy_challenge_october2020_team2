47b4ab6ab30cab872eb774c34404aef5d5fcb75c
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-13 15:55:35 +0100

    Adding second test to user can see notifications when emails arrive

--- spec/features/user_can_see_notifications_when_emails_arrive_spec.rb
+++ spec/features/user_can_see_notifications_when_emails_arrive_spec.rb
@@ -8,5 +8,14 @@ feature 'User can see notifications when emails arrive' do
     visit mailbox_inbox_path
     expect(page).to have_link '1 Inbox', href: '/mailbox/inbox'
   end
+  
+  it 'when receiver reload current page' do
+    login_as(receiver, scope: :user)
+    visit mailbox_inbox_path
+    expect(page).to have_link 'Inbox', href: '/mailbox/inbox'
+    sender.send_message(receiver, 'Message', 'Title')
+    visit current_path
+    expect(page).to have_link '1 Inbox', href: '/mailbox/inbox'
+  end
 end
  
