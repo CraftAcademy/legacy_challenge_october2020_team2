deec7828b89235643700bb316320b062bd0c6d67
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-13 15:54:10 +0100

    Creating a new feature branch and writing test for users can see notifications

---
+++ spec/features/user_can_see_notifications_when_emails_arrive_spec.rb
@@ -0,0 +1,12 @@
+feature 'User can see notifications when emails arrive' do
+  let(:sender) { FactoryBot.create(:user, name:'Tom', email: 'tom@gmail.com') }
+  let(:receiver) { FactoryBot.create(:user, name:'Tim', email: 'tim@gmail.com') }
+
+  it 'when receiver login and click on inbox' do
+    sender.send_message(receiver, 'Message', 'Title')
+    login_as(receiver, scope: :user)
+    visit mailbox_inbox_path
+    expect(page).to have_link '1 Inbox', href: '/mailbox/inbox'
+  end
+end
+ 
